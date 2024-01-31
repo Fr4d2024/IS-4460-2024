771571eb90431c2b6e8e86ff02ddc6a06c3b168d
Fr4d2024 <ramos.f.1023@gmail.com>
2024-01-31 13:20:56 -0700

    Python Code!

---
+++ basic Python/basic_python_Ramos_Fred.py
@@ -0,0 +1,34 @@
+print("Hello World!")
+
+a = 5
+b = 6
+
+print(a+b)
+
+A = 5
+a = 6
+print(a)
+print(A)
+
+print(type(A))
+print(type(4.0))
+
+user_name = "Bob"
+print("My name is " + user_name + " actually!")
+print(f"My name is {user_name} actually!")
+
+name = "Jake"
+
+def add_numbers(a,b):
+  print(a+b)
+  #print(P)
+  print(b)
+
+add_numbers(b=6,a=10)
+
+def say_hello():
+  name = "Fred"
+
+  print(name)
+
+print(name)
