f21d5cc38722ac9f2db42088f339556d1c4f2fd6
Fr4d2024 <ramos.f.1023@gmail.com>
2024-03-07 22:21:32 -0700

    YES!

--- Lab_6/model_lab/customer/models.py
+++ Lab_6/model_lab/customer/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     total_items = models.IntegerField()
-    order_date = models.DateField()
+    order_date = models.DateField(default=timezone.now().date())
 # Create your models here.
