6a209403aee50a490f30619924e6b8b8fc663c34
Fr4d2024 <ramos.f.1023@gmail.com>
2024-03-08 21:43:44 -0700

    YEAHHH!

--- HW2/models_cinemark/movie/models.py
+++ HW2/models_cinemark/movie/models.py
@@ -2,22 +2,26 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    director = models.CharField(max_length=100)
-    release_year = models.CharField(max_length=50)
-    budget = models.CharField(max_length=50)
-    runtime = models.CharField(max_length=50)
-    rating = models.CharField(max_length=50)
-    genre = models.CharField(max_length=50)
+    title = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=500, blank=True)
+    director = models.CharField(max_length=100,blank=True)
+    release_year = models.CharField(max_length=50,blank=True)
+    budget = models.CharField(max_length=50,blank=True)
+    runtime = models.CharField(max_length=50,blank=True)
+    rating = models.CharField(max_length=50,blank=True)
+    genre = models.CharField(max_length=50,blank=True)
 
 
 class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=255, null=False)
+    first_name = models.CharField(max_length=50,blank=True)
+    last_name = models.CharField(max_length=50,blank=True)
+    email = models.CharField(max_length=100,blank=True)
+
+class Role(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', null=False)
+    role = models.CharField(max_length=50, null=False)
 
 
 
